90a6506604216c546468264298b90220ffd49631
solinehayes <hayessoline@gmail.com>
2020-03-29 17:28:44 +0200

    :package: (FireBase) Installing and configurating firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
 import { RootNavigator } from "./navigation/Navigator";
+
+import * as firebase from "firebase";
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAvHczJ7pVkKnG3UsuXix13YkNFPud60fY",
+  authDomain: "to-do-b2263.firebaseapp.com",
+  databaseURL: "https://to-do-b2263.firebaseio.com/",
+  storageBucket: "gs://to-do-b2263.appspot.com",
+};
+
+firebase.initializeApp(firebaseConfig);
 
 export default function App() {
   return <RootNavigator />;
